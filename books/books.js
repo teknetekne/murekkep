ad423d8824927a591319aaf1110c576d717afe6f
emre tekneci <79002900+teknetekne@users.noreply.github.com>
2024-06-09 00:38:47 +0300

    Add files via upload

---
+++ books/books.js
@@ -0,0 +1,55 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    autoplay: {
+      delay:4000,
+      disableOnInteraction: false,
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
+  function toggleMenu() {
+    var x = document.getElementById("hamburger-navbar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  
+    var x = document.getElementById("main");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function closeMenu() {
+    var x = document.getElementById("hamburger-navbar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  
+    var x = document.getElementById("main");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
